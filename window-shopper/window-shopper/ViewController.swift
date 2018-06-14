b2be0219a4ec8fdde5b2082a5892a5eb786c6d83
Hyeon Uk Jeong <hyeonukjeong@Hyeons-MacBook-Pro.local>
2018-06-14 16:13:49 +0900

    started window shopper

---
+++ window-shopper/window-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Hyeon Uk Jeong on 6/14/18.
+//  Copyright © 2018 Hyeon Uk Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
